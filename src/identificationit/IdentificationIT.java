a9f128fc808b8bb2eb41200078eb9066190b11d9
whilda chaq <whildachaq@gmail.com>
2014-09-15 11:11:18 +0700

    Ending

--- src/identificationit/IdentificationIT.java
+++ src/identificationit/IdentificationIT.java
@@ -6,8 +6,12 @@ package identificationit;
  */
 
 import java.io.*;
+import static java.lang.Thread.sleep;
 import java.net.URI;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Comparator;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
@@ -15,6 +19,7 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 import org.apache.hadoop.conf.*;
 import org.apache.hadoop.fs.*;
+import org.apache.hadoop.util.GenericOptionsParser;
 import sourceafis.simple.AfisEngine;
 import sourceafis.simple.Fingerprint;
 import sourceafis.simple.Person;
@@ -35,7 +40,7 @@ public class IdentificationIT{
                 }
             }
         }
-        
+                
         public static AfisEngine FIS = new AfisEngine();
         public static Fingerprint fpQuery = new Fingerprint();
         public static Person queryPerson = new Person(fpQuery);
@@ -48,16 +53,63 @@ public class IdentificationIT{
         
         public static void main (String [] args) throws Exception{
                 try{
-                    int K = 20000;
-                    float Threshold = -2;
-                    String db = "hdfs://localhost:9000/db/zip-Summary";
-                    String pathQuery = "hdfs://localhost:9000/query/FVC2000-Db1_a_1_1.template";
+                    Date startDate = new Date();
+                    MeasurementThread mr = new MeasurementThread("identificationIT", Runtime.getRuntime());
+                    mr.start();
+                    sleep(500);
+                    Configuration conf = new Configuration();
+                    String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+                    String paramK = "";
+                    String paramThreshold = "";
+                    String paramPathQuery = "";
+                    String paramPathInput = "";
+                    String paramPathOutput = "";
+                    if (otherArgs.length == 4) {
+                        paramK = otherArgs[0];
+                        paramThreshold = otherArgs[1];
+                        if(otherArgs[2].equals("1")){
+                            paramPathQuery = "/Query/FVC2000-Db1_b_105_8.template";
+                            paramPathInput = "/Percobaan1";
+                        }else if(otherArgs[2].equals("2")){
+                            paramPathQuery = "/Query/FVC2002-Db4_a_22_4.template";
+                            paramPathInput = "/Percobaan2";
+                        }else if(otherArgs[2].equals("3")){
+                            paramPathQuery = "/Query/FVC2004-Db3_a_99_3.template";
+                            paramPathInput = "/Percobaan3";
+                        }else if(otherArgs[2].equals("4")){
+                            paramPathQuery = "/Query/FVC2006-Db2_a_84_11.template";
+                            paramPathInput = "/Percobaan4";
+                        }
+                        paramPathOutput = otherArgs[3];
+                    }else if (otherArgs.length == 5) {
+                        paramK = otherArgs[0];
+                        paramThreshold = otherArgs[1];
+                        paramPathQuery = otherArgs[2];
+                        paramPathInput = otherArgs[3];
+                        paramPathOutput = otherArgs[4];
+                    }else{
+                        System.err.println("Usage: IdentificationIT <K> <Th> <Q> <in> <out>");
+                        System.err.println("K: number for ouput, ex : 5");
+                        System.err.println("Th: threshold for matching score, ex : 10");
+                        System.err.println("Q: Path to query template, ex : /dir/a.template");
+                        System.err.println("in: Path to directory input, ex : /dir");
+                        System.err.println("out: Path to file output, ex : /dir");
+                        System.exit(2);
+                    }
+                    
+                    int K = Integer.parseInt(paramK);
+                    float Threshold = Float.parseFloat(paramThreshold);
+                    String pathQuery = "hdfs://master:9000"+paramPathQuery;
+                    String db = "hdfs://master:9000"+paramPathInput;
+                    String output = paramPathOutput;
+                    
                     float matchingScore;
-                    FileSystem hdfs = FileSystem.get( new URI( "hdfs://localhost:9000" ), new Configuration());     
+                    
+                    FileSystem hdfs = FileSystem.get( new URI( "hdfs://master:9000" ), conf);     
                     SetupQueryPerson(hdfs,pathQuery);
                     FileStatus[] statusList = hdfs.listStatus(new Path(db));
                     
-                    float startTime = System.currentTimeMillis();
+                    long startTime = System.currentTimeMillis();
                     int counter = 0;
                     for (FileStatus status : statusList) {
                         if(!status.getPath().getName().contains("properties")){
@@ -66,7 +118,6 @@ public class IdentificationIT{
                                 SetupDataPerson(template,status.getPath());
                                 matchingScore = FIS.verify(queryPerson, dataPerson);
                                 if(matchingScore >= Threshold){
-                                //if(matchingScore != 0.0){
                                     map.put(status.getPath().getName(), matchingScore);
                                 }
                                 System.out.println("Progress : "+ ((float) ++counter/statusList.length) * 100 +" %");
@@ -78,16 +129,25 @@ public class IdentificationIT{
                     
                     sorted_map.putAll(map);
                     int i = 0;
-                    for(Map.Entry<String,Float> entry : map.entrySet()) {
-                        try(PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("/home/whilda/info", true)))) {
+                    for(Map.Entry<String,Float> entry : sorted_map.entrySet()) {
+                        try(PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(output, true)))) 
+                        {
                             out.println(entry.getKey()+"\t"+entry.getValue());
                         }catch (IOException e) {
                             System.out.println("Error write : "+e.getMessage());
                         }
                         if(++i == K) break;
                     }
-                    float endTime = System.currentTimeMillis();
-                    System.out.println("Time : "+((float)(endTime-startTime))+" s");
+                    Date endDate = new Date();
+                    mr.Stop();
+                    
+                    DateFormat df = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+                    System.out.println("Start\t: "+ df.format(startDate));
+                    System.out.println("Finish\t: "+ df.format(endDate));
+
+                    long diff = endDate.getTime() - startDate.getTime();
+                    System.out.println("Time (ms)\t: "+ diff);
+                    mr.PrintAverage();
                 }catch(IOException | IllegalArgumentException e){
                     System.out.println("ERROR : "+e.getMessage());
                 }
